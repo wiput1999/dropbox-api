f823632cf48a515296473b8431c77006877a631a
Wiput Pootong <wiput.pootong@gmail.com>
2022-08-05 00:56:32 +0700

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { PrismaService } from './prisma.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors()
+
   const prismaService: PrismaService = app.get(PrismaService)
   await prismaService.enableShutdownHooks(app)
 
