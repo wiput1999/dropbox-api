3e49b4b1600aea90ecfef0345fb999fee2d55923
Wiput Pootong <wiput.pootong@gmail.com>
2022-08-18 00:57:58 +0700

    feat(profile): findUnique

--- src/user/profile.service.ts
+++ src/user/profile.service.ts
@@ -1,10 +1,20 @@
 import { ForbiddenException, Injectable } from '@nestjs/common'
 import { CreateProfileInput } from './entity/profile.entity'
-import { PrismaService } from '@/db/prisma.service'
+import { PrismaService } from '@/db'
 
 @Injectable()
 export class ProfileService {
   constructor(private readonly prisma: PrismaService) {}
+
+  findUnique(userId: string) {
+    return this.prisma.user
+      .findUnique({
+        where: {
+          id: userId,
+        },
+      })
+      .profile()
+  }
 
   async createProfile(userId: string, input: CreateProfileInput) {
     const user = await this.prisma.user.findUnique({
